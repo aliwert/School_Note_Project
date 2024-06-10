0d23fa6dc7922226a09115b58ab276540a2b2696
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-10 19:26:58 +0300

    club deletion process has been completed

--- School_Note_Project/FrmClub.Designer.cs
+++ School_Note_Project/FrmClub.Designer.cs
@@ -28,17 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmClub));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtClubid = new System.Windows.Forms.TextBox();
+            this.TxtClubName = new System.Windows.Forms.TextBox();
             this.BtnList = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -49,6 +52,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(704, 186);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // label1
             // 
@@ -68,19 +72,20 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Club Name:";
             // 
-            // textBox1
+            // TxtClubid
             // 
-            this.textBox1.Location = new System.Drawing.Point(327, 319);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(348, 31);
-            this.textBox1.TabIndex = 3;
+            this.TxtClubid.Enabled = false;
+            this.TxtClubid.Location = new System.Drawing.Point(327, 319);
+            this.TxtClubid.Name = "TxtClubid";
+            this.TxtClubid.Size = new System.Drawing.Size(348, 31);
+            this.TxtClubid.TabIndex = 3;
             // 
-            // textBox2
+            // TxtClubName
             // 
-            this.textBox2.Location = new System.Drawing.Point(327, 387);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(348, 31);
-            this.textBox2.TabIndex = 4;
+            this.TxtClubName.Location = new System.Drawing.Point(327, 387);
+            this.TxtClubName.Name = "TxtClubName";
+            this.TxtClubName.Size = new System.Drawing.Size(348, 31);
+            this.TxtClubName.TabIndex = 4;
             // 
             // BtnList
             // 
@@ -101,23 +106,25 @@
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // BtnAdd
             // 
-            this.button3.Location = new System.Drawing.Point(215, 512);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(109, 30);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Add";
-            this.button3.UseVisualStyleBackColor = true;
+            this.BtnAdd.Location = new System.Drawing.Point(215, 512);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(109, 30);
+            this.BtnAdd.TabIndex = 7;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
-            // button4
+            // BtnDelete
             // 
-            this.button4.Location = new System.Drawing.Point(545, 512);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(109, 30);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
+            this.BtnDelete.Location = new System.Drawing.Point(545, 512);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(109, 30);
+            this.BtnDelete.TabIndex = 8;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.button4_Click);
             // 
             // label3
             // 
@@ -129,19 +136,34 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "CLUB OPERATION PANEL";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(894, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
             // FrmClub
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(960, 554);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnAdd);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.BtnList);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtClubName);
+            this.Controls.Add(this.TxtClubid);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -153,6 +175,7 @@
             this.Text = "FrmClub";
             this.Load += new System.EventHandler(this.FrmClub_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,12 +186,13 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtClubid;
+        private System.Windows.Forms.TextBox TxtClubName;
         private System.Windows.Forms.Button BtnList;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnDelete;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- School_Note_Project/FrmClub.cs
+++ School_Note_Project/FrmClub.cs
@@ -34,5 +34,48 @@ namespace School_Note_Project
         {
             list();
         }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cm = new SqlCommand("insert into tbl_clubs (clubname) values (@p1)", con);
+            cm.Parameters.AddWithValue("@p1", TxtClubName.Text);
+            cm.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Club added to list", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            list();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.LightBlue;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Transparent;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            TxtClubid.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            TxtClubName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand("Delete From Tbl_Clubs WHERE CLUBSID=@P1", con);
+            com.Parameters.AddWithValue("@p1", TxtClubid.Text);
+            com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Succesfully deleted");
+            list();
+        }
     }
 }
